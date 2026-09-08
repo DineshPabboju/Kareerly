485ba64df6b77d826b74d2c07e65e0966ba0e0e5
Dinesh Pabboju <dinesh040805@gmail.com>
2026-09-08 18:09:49 +0530

    Generated an Example Board Layout

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,26 +1,8 @@
-import { useEffect, useState } from 'react';
-import fetchData from './api/api'
+import Board from './components/Board'
 
 const App = () => {
-  const [data, setData] = useState(null);
-
-  useEffect(()=> {
-    const loadData = async () => {
-      try{
-        const result = await fetchData();
-        // const response = result.json;
-        console.log(result)
-        setData(result)
-      }
-      catch(error){
-        console.log(error)
-      }
-    };
-    loadData();
-  }, [])
   return (
-    
-    <div>Hello {data && <pre>{JSON.stringify(data, null, 2)}</pre>}</div>
+    <Board />
   )
 }
 
--- frontend/src/components/CardModal.tsx
+++ frontend/src/components/CardModal.tsx
@@ -1,5 +1,3 @@
-import React from 'react'
-
 const CardModal = () => {
   return (
     <div>CardModal</div>
